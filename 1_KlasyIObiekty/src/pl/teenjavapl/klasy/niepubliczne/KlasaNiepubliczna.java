a94ccd9d918ca0c0e94de45ffd8f348647667904
PolishCivil <softnyxscripts@gmail.com>
2013-06-22 17:51:06 +0200

    rename -.-

---
+++ 1_KlasyIObiekty/src/pl/teenjavapl/klasy/niepubliczne/KlasaNiepubliczna.java
@@ -0,0 +1,14 @@
+package pl.teenjavapl.klasy.niepubliczne;
+
+/**
+ * 
+ * @author PolishCivil Klasa niepubliczna różni się od publicznej tym, że nie
+ *         będziemy mogli z niej korzystać tylko w pakiecie w którym się
+ *         znajduje. Czyli w przypadku tej klasy możemy ją używać tylko w
+ *         pl.teenjavapl.klasy.niepubliczne
+ *         w pozostałych będzie 'niewidoczna'
+ */
+class KlasaNiepubliczna {
+    int field1, field2;
+    String str1, str2;
+}
